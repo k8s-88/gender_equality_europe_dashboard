000a5429653cd9eb62816c897256cf881b65645b
Katie O Donnell <katiemodonnell88@gmail.com>
2019-01-01 12:51:52 +0000

    equality index graph added to JS

--- graphs.js
+++ graphs.js
@@ -22,6 +22,48 @@ function makeGraph(error, transactionsData) {
     }
 
 
+equalityIndexFunction (ndx, "#indexScore")
+
+
+
+    // --------------------EQUALITY INDEX AS BAR CHART------------------------
+
+    function equalityIndexFunction (ndx, banana) {
+        
+    
+        
+    let countryDimEqualityIndex = ndx.dimension(dc.pluck("country"));
+
+    let indexScore = countryDimEqualityIndex.group().reduceSum(dc.pluck("equality_index"));
+
+    let equalityIndex = dc.barChart(banana);
+
+    let equalityIndexcolors = d3.scale.ordinal().range(["cyan", "lightpink", "lightgreen", "yellow", "lightblue"]);
+
+
+    equalityIndex
+        .width(chartWidth * 4)
+        .height(500)
+        .margins({ top: 10, right: 20, bottom: 50, left: 50 })
+        .colors(equalityIndexcolors)
+        .colorAccessor(function(d) {
+            return d.key
+        })
+        .dimension(countryDimEqualityIndex)
+        .group(indexScore)
+        .yAxisLabel("Gender Equality Index Score")
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        // .xAxisLabel("country")
+        .elasticY(true)
+        .yAxis().ticks(10)
+
+}
+        
+
+
+
+   
 
 
 
