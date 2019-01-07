459eaaa7a83a2e300fc8f64a6496ee0587e763e8
Katie O Donnell <katiemodonnell88@gmail.com>
2019-01-07 08:29:23 +0000

    question 1 to 3 graphs added to questions.js

--- questions.js
+++ questions.js
@@ -15,6 +15,92 @@ function makeGraph(error, transactionsData) {
     let height = 400;
 
 
+    // -----------------------QUESTION 1------------------------------------
+
+
+    countryDimAgreement = ndx.dimension(dc.pluck('country'));
+
+    let Disagree = countryDimAgreement.group().reduceSum(dc.pluck("q1_disagree"));
+
+    var Agree = countryDimAgreement.group().reduceSum(
+        function(d) {
+            return 100 - d['q1_disagree'];
+        }
+    );
+
+    var question1Chart = dc.barChart("#question1Rank");
+
+    question1Chart
+        .width(chartWidth * 4)
+        .height(height)
+        .dimension(countryDimAgreement)
+        .group(Disagree)
+        .stack(Agree)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal);
+
+
+
+    // -----------------------QUESTION 2------------------------------------
+
+
+
+    countryDimAgreement2 = ndx.dimension(dc.pluck('country'));
+
+    let Disagree2 = countryDimAgreement2.group().reduceSum(dc.pluck("q2_disagree"));
+
+    var Agree2 = countryDimAgreement2.group().reduceSum(
+        function(d) {
+            return 100 - d['q2_disagree'];
+        }
+    );
+
+    var question2Chart = dc.barChart("#question2Rank");
+
+    question2Chart
+        .width(chartWidth * 4)
+        .height(height)
+        .dimension(countryDimAgreement2)
+        .group(Disagree2)
+        .stack(Agree2)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal);
+
+
+
+
+
+
+    // -----------------------QUESTION 3------------------------------------
+
+
+    countryDimAgreement3 = ndx.dimension(dc.pluck('country'));
+
+    let Disagree3 = countryDimAgreement3.group().reduceSum(dc.pluck("q3_disagree"));
+
+    var Agree3 = countryDimAgreement3.group().reduceSum(
+        function(d) {
+            return 100 - d['q3_disagree'];
+        }
+    );
+
+    var question3Chart = dc.barChart("#question3Rank");
+
+    question3Chart
+        .width(chartWidth * 4)
+        .height(height)
+        .dimension(countryDimAgreement3)
+        .group(Disagree3)
+        .stack(Agree3)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal);
+
+
+
+    // -----------------------------------------------------------------------
+
+
+
 
 
     dc.renderAll();
